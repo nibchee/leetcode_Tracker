f64ca5cb636078a8efd279ee85a28aca12560425
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-06 20:39:35 +0530

    Time: 45 ms (20.31%) | Memory: 55.9 MB (42.04%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,58 +1,61 @@
 class Solution {
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
-        int n=position.length;
-        int min=0;
-        int max=position[n-1];
-        int ans=0;
-        while(min<=max){
-            int mid=min+(max-min)/2;
+        int n = position.length;
+        int min = Integer.MAX_VALUE;
+        int max = position[n - 1] - position[0];
+        // min distance can be min between any 2 consecutive balls
+        for (int i = 1; i < n; i++) {
+            min = Math.min(min, position[i] - position[i - 1]);
+        }
 
-            if(is_possible(position,m,mid)){
-                ans=mid;
-                min=mid+1;
-            }else{
-                max=mid-1;
+        int ans = 0;
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+
+            if (is_possible(position, m, mid)) {
+                ans = mid;
+                min = mid + 1;
+            } else {
+                max = mid - 1;
             }
         }
 
-        //now we have got the distance
-        //now cal force
-        //System.out.println(ans);
-        //return force(position,ans);
+        // now we have got the distance
+        // now cal force
+        // System.out.println(ans);
+        // return force(position,ans);
         return ans;
     }
 
-    private int force(int[] position,int dist){
-        int force=Integer.MAX_VALUE;
-         int last_placed=position[0];
+    private int force(int[] position, int dist) {
+        int force = Integer.MAX_VALUE;
+        int last_placed = position[0];
 
-      for(int i=1;i<position.length;i++){
-        if(position[i]-last_placed>=dist){
-            force=Math.min(position[i]-last_placed,force);
-            last_placed=position[i];
+        for (int i = 1; i < position.length; i++) {
+            if (position[i] - last_placed >= dist) {
+                force = Math.min(position[i] - last_placed, force);
+                last_placed = position[i];
+            }
         }
+        return force;
     }
-    return force;
+
+    private boolean is_possible(int[] position, int m, int dist) {
+        // place first ball
+        int last_placed = position[0];
+        int balls = 1;
+
+        for (int i = 1; i < position.length; i++) {
+            if (position[i] - last_placed >= dist) {
+                last_placed = position[i];
+                balls++;
+            }
+
+            if (balls == m)
+                return true;
+        }
+        return false;
     }
-
-    private boolean is_possible(int[] position,int m, int dist){
-      //place first ball
-      int last_placed=position[0];
-      int balls=1;
-
-      for(int i=1;i<position.length;i++){
-        if(position[i]-last_placed>=dist){
-            last_placed=position[i];
-            balls++;
-        }
-
-        if(balls==m)
-        return true;
-        }
-      return false;
-      }
-    
-
 
 }
