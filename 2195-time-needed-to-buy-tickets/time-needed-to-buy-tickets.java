8c712e697b805acc12657cd08d807310093e828e
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-09 18:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.65%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,21 +1,17 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-      int count=0;
-    while(true){
-        boolean finish=false;
-        for(int i=0;i<tickets.length;i++){
-            if(tickets[i]==0)
-            continue;
-            tickets[i]--;
-            count++;
-            if(tickets[k]==0){
-                finish=true;
-                break;
+      int time = 0;
+        
+        for (int i = 0; i < tickets.length; i++) {
+            // If the current person is before or at the desired person 'k'
+            if (i <= k) {
+                time += Math.min(tickets[k], tickets[i]);
+            } else {
+                time += Math.min(tickets[k] - 1, tickets[i]);
             }
         }
-        if(finish)break;
-    }
-    return count;
+        
+        return time;
     }
    
 }
