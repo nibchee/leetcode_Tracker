fde9ac254bfd63c2b490c349a450c13335d76796
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-11 22:34:24 +0530

    Time: 145 ms (56.94%) | Memory: 41.4 MB (70.08%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,69 +1,33 @@
 class Solution {
     boolean visited[][];
+
     public boolean exist(char[][] board, String word) {
-        boolean found=false;
-         char start_letter=word.charAt(0);
-         visited=new boolean[board.length][board[0].length];
-         for(int i=0;i<board.length;i++){
-             for(int j=0;j<board[i].length;j++){
-                 if(board[i][j]==start_letter){
-                     visited[i][j]=true;
-                     if(search_word(i,j,board,word.substring(1),""))
-                     {
-                         found=true;
-                         break;
-                     }
-                     visited[i][j]=false;
-                 }
-                 if(found)
-                 break;
-             }
-         }  
-        return found; 
+        int m = board.length;
+        int n = board[0].length;
+        visited = new boolean[m][n];
+        boolean p = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word.charAt(0) == board[i][j] && dfs(board, i, j, word, 0))
+                    return true;
+            }
+        }
+        return false;
     }
 
-    public boolean search_word(int x,int y,char[][] board, String word,String path){
-       //   System.out.println("Word-"+word);
-      if(word.length()==0){
-      //    System.out.println("Path "+path);
-          return true;
-      }
-         char start_letter=word.charAt(0);
-         //Move Left
-         if(y-1>=0 && !visited[x][y-1] && board[x][y-1]==start_letter){
-             visited[x][y-1]=true;
-            if(search_word(x,y-1,board,word.substring(1),path+"L")){
-                 return true;
-            }
-             visited[x][y-1]=false; 
-         }
+    public boolean dfs(char[][] board, int i, int j, String word, int idx) {
+        if (idx == word.length())
+            return true;
 
-         //Move Right
-          if(y+1<board[0].length && !visited[x][y+1] && board[x][y+1]==start_letter){
-             visited[x][y+1]=true;
-             if(search_word(x,y+1,board,word.substring(1),path+"R")){
-                 return true;
-             }
-             visited[x][y+1]=false; 
-         }
-         //Move Up
-      if(x-1>=0 && !visited[x-1][y] && board[x-1][y]==start_letter){
-             visited[x-1][y]=true;
-             if(search_word(x-1,y,board,word.substring(1),path+"U")){
-                 return true;
-             }
-             visited[x-1][y]=false; 
-         }
-         //Move Down
-         if(x+1<board.length && !visited[x+1][y] && board[x+1][y]==start_letter){
-             visited[x+1][y]=true;
-             if(search_word(x+1,y,board,word.substring(1),path+"D")){
-                 return true;
-             }
-             visited[x+1][y]=false; 
-            }
-      return false;
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]
+                || board[i][j] != word.charAt(idx))
+            return false;
 
+        visited[i][j] = true;
+        boolean found = dfs(board, i + 1, j, word, idx + 1) || dfs(board, i - 1, j, word, idx + 1)
+                || dfs(board, i, j + 1, word, idx + 1) || dfs(board, i, j - 1, word, idx + 1);
+        visited[i][j] = false;
+        return found;
     }
 
 }
