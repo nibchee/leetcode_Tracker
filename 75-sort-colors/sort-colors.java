26d35ba4f883857405bd16cba7be1fe71b243bde
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-14 16:52:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (49.05%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,28 +1,30 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        //Just take 3 pointers
-        int l=0,m=0,r=nums.length-1;
-        while(m<=r){
-            switch(nums[m]){
-                case 0:
-                  swap(nums,m,l);
-                  m++;
-                  l++;
-                  break;
-                case 1:
-                 //just increment mid
-                  m++;
-                  break;
-                case 2:
-                  swap(nums,r,m);
-                  r--;
-            }
-        }
-    }
-
-    public void swap(int[] arr,int i,int j){
+    private void swap(int arr[],int i,int j){
         int temp=arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
     }
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int l=0,m=0,r=n-1;
+        while(m<=r){
+            switch(nums[m]){
+                case 0:
+                   swap(nums,m,l);
+                   l++;
+                   m++;
+                   break;
+                case 1:
+                    m++;
+                  break;
+                case 2:
+                swap(nums,m,r);
+                r--;
+                break;
+            }
+        }
+
+    }
+
+
 }
