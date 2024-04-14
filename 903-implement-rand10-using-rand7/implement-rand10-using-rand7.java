1e2cd62562e488194bb012c9844b17c09ad40ea5
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-04-14 14:34:19 +0530

    Time: 5 ms (87.11%) | Memory: 49.3 MB (43.03%) - LeetSync

--- 903-implement-rand10-using-rand7/implement-rand10-using-rand7.java
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.java
@@ -3,15 +3,21 @@
  * public int rand7();
  * @return a random integer in the range 1 to 7
  */
+     /* Here as we need to generate 10 numbers with equal probabilty 
+          so can't use only rand7() once but have to use twice
+          so Create a Table with 7*7 size , 
+          & Map values from 1-40 , 
+          Formula for nth ele= (R-1)*7 + C
+          so val=(Rand7()-1)*7+Rand7()
+          if its val > 40 then again generate
+        */
+    
 class Solution extends SolBase {
     public int rand10() {
-        int a=rand7();
-        int b=rand7();
-        int val=(a-1)*7+b;
+        int val=(rand7()-1)*7+rand7();
         while(val>40){
             val=(rand7()-1)*7+rand7();
         }
-      return val%10+1;
-
+        return val%10+1; //as have to gen 1-10
     }
 }
