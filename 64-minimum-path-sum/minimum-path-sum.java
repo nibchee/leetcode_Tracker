f8b8ee799a0b175ac013797039468465047245ad
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-05-29 02:00:33 +0530

    Time: 4 ms (33.48%) | Memory: 45.2 MB (82.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -4,7 +4,20 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         dp=new int[m][n];
-        return helper(m-1,n-1,grid);
+
+        for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+            if(i==0 && j==0){
+                dp[i][j]=grid[i][j];
+            }else{
+                int left=(j-1)>=0? dp[i][j-1]:Integer.MAX_VALUE;
+                int up=(i-1)>=0?dp[i-1][j]:Integer.MAX_VALUE;
+                dp[i][j]=Math.min(left,up)+grid[i][j];
+            }
+           }
+        }
+        //return helper(m-1,n-1,grid);
+        return dp[m-1][n-1];
     }
 
     private int helper(int i,int j,int[][]grid){
