5112ecf6a422ba206dda80cfb35eb12fded53820
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-05-29 00:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (42.57%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -6,7 +6,27 @@ class Solution {
         dp=new int[m][n];
        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1)
         return 0;
-       return helper(m-1,n-1,m,n,obstacleGrid); 
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                dp[i][j]=0;
+                continue;
+                }
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }else{
+                        int left=(j-1)>=0 ? dp[i][j-1]:0;
+                        int up=(i-1)>=0 ?dp[i-1][j]:0;
+                       // System.out.println(left+" L "+up+" U ");
+                        dp[i][j]=left+up;
+                }
+               // System.out.print(dp[i][j]+" ");
+            }
+           // System.out.println();
+        }
+       //return helper(m-1,n-1,m,n,obstacleGrid); 
+       return dp[m-1][n-1];
     }
     private int helper(int i,int j,int m,int n,int[][]obstacleGrid){
 
