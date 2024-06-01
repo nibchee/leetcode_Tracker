bf65ea26eb0e059c6b9cdc569cfbf9194cda1ee1
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-01 15:40:32 +0530

    Time: 20 ms (76.26%) | Memory: 50.8 MB (38.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -16,8 +16,35 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         dp = new int[n][m];
-        for (int[] arr : dp)
-            Arrays.fill(arr, -1);
-        return findLongest(n - 1, m - 1, text1, text2);
+        // for (int[] arr : dp)
+        // Arrays.fill(arr, -1);
+        // return findLongest(n - 1, m - 1, text1, text2);
+        dp[0][0] = 0;
+        if (text1.charAt(0) == text2.charAt(0))
+            dp[0][0] = 1;
+        // filling 1st column
+        for (int i = 1; i < n; i++) {
+            if (text1.charAt(i) == text2.charAt(0))
+                dp[i][0] = 1;
+            else
+                dp[i][0] = dp[i - 1][0];
+        }
+        // filling 1st row
+        for (int j = 1; j < m; j++) {
+            if (text1.charAt(0) == text2.charAt(j))
+                dp[0][j] = 1;
+            else
+                dp[0][j] = dp[0][j-1];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (text1.charAt(i) == text2.charAt(j))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n - 1][m - 1];
     }
 }
