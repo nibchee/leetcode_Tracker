62a19339aa993a0c7849aeeb8ee125ae9688b9c9
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-02 15:03:48 +0530

    Time: 6 ms (16.49%) | Memory: 44.9 MB (53.31%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,5 +1,6 @@
 class Solution {
     int dp[][];
+
     private int helper(int i, int j, String word1, String word2) {
         // word1 got exhausted
         if (i < 0)
@@ -9,12 +10,13 @@ class Solution {
         if (j < 0)
             return i + 1; // I+1 deletion operations
 
-            if(dp[i][j]!=-1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         if (word1.charAt(i) == word2.charAt(j))
-            return dp[i][j]=helper(i - 1, j - 1, word1, word2);
+            return dp[i][j] = helper(i - 1, j - 1, word1, word2);
 
-        return dp[i][j]=1 + Math.min(
+        return dp[i][j] = 1 + Math.min(
                 helper(i, j - 1, word1, word2), /** Insertion */
                 Math.min(helper(i - 1, j, word1, word2), /** Deletion */
                         helper(i - 1, j - 1, word1, word2)/** Replace */
@@ -24,9 +26,37 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        dp=new int[m][n];
-        for(int[] arr:dp)
-        Arrays.fill(arr,-1);
-        return helper(m - 1, n - 1, word1, word2);
+        if(m==0) return n;
+        if(n==0) return m;
+
+        // dp = new int[m][n];
+        // for (int[] arr : dp)
+        // Arrays.fill(arr, -1);
+        // return helper(m - 1, n - 1, word1, word2);
+
+        // Applying Tabulation & f(0,0) ,f(0,1) => Shifting
+        dp = new int[m + 1][n + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i <=m; i++) {
+            dp[i][0] = 1 + dp[i - 1][0];
+        }
+
+        for(int i=1;i<=n;i++)
+         dp[0][i] = 1 + dp[0][i - 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // shifiting
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(
+                            dp[i][j - 1], /** Insertion */
+                            Math.min(dp[i - 1][j], /** Deletion */
+                                    dp[i - 1][j - 1]/** Replace */
+                            ));
+            }
+        }
+        return dp[m][n];
     }
 }
