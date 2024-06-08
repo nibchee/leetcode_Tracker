415f1a885a3762d04f35599b1f9ccc881f625d42
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-09 00:41:12 +0530

    Time: 38 ms (67.21%) | Memory: 51.9 MB (43.40%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,28 +1,36 @@
 class Solution {
-    int dp[][];
-    private int helper(int idx,int sum,int nums[]){
-       if(sum<0 || idx<0) return 0;
-       if(sum==0) return 1;
-       if(idx==0) return (nums[idx]==sum)?1:0;
-
-       if(dp[idx-1][sum-1]!=-1) return dp[idx-1][sum-1];
-
-
-       return dp[idx-1][sum-1]=(helper(idx-1,sum,nums)==1)||(helper(idx-1,sum-nums[idx],nums)==1)?1:0;
-    }
     public boolean canPartition(int[] nums) {
-        /** The trick of this ques is a subset can be partitioned in 2 equal parts if
-        sum is even i.e can be divided in 2 halves & then just find if subset exists with sum =(Totalsum/2)*/
-        int sum =0;
-        for(int i=0;i<nums.length;i++)
-        sum+=nums[i];
+        /**
+         * The trick of this ques is a subset can be partitioned in 2 equal parts if
+         * sum is even i.e can be divided in 2 halves & then just find if subset exists
+         * with sum =(Totalsum/2)
+         */
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++)
+            sum += nums[i];
+        if (sum % 2 == 1)
+            return false;
+        int n = nums.length;
+        boolean[][] dp = new boolean[n][sum + 1];
+        sum = sum / 2;
+        for (int i = 0; i < n; i++)
+            dp[i][0] = true;
 
-        if(sum%2==1)return false;
-       int n=nums.length-1;
-       dp=new int[n][sum];
-       for(int[] arr:dp){
-        Arrays.fill(arr,-1);
-       }
-       return  helper(n-1,sum/2,nums)==1?true:false;  
+        for (int j = 1; j <= sum; j++) {
+            if (nums[0] == j)
+                dp[0][j] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                boolean notTake = dp[i - 1][j];
+                boolean Take = (j - nums[i] >= 0) ? dp[i - 1][j - nums[i]] : false;
+                dp[i][j] = notTake | Take;
+                // System.out.print(dp[i][j] + " ");
+            }
+            // System.out.println();
+        }
+
+        return dp[n - 1][sum];
     }
 }
