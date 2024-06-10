f913a7d307ab3b9ae1d053e04c89949e5ea38feb
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-10 14:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (84.42%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -5,8 +5,8 @@ class Solution {
         long rem=n%2;
         long even=d+rem;
         long odd=d;
-        long res1=pow(5,even)%MOD;
-        long res2=pow(4,odd)%MOD;
+        long res1=pow2(5,even)%MOD;
+        long res2=pow2(4,odd)%MOD;
         long res= (res1*res2)%MOD;
         return (int)res;
     }
@@ -23,5 +23,18 @@ class Solution {
             }
         }
         return res;
+    }
+
+     public long pow2(long a,long b){
+        if(b==0){
+            return 1;
+        }
+        long temp=pow2(a,b/2);
+        if(b%2==0){
+            return (temp*temp)%MOD;
+        }
+        else{
+            return (a *temp*temp)%MOD;
+        }
     }
 }
