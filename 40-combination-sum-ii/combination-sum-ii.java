60f02e584d3397cd815eb1793f949da0b9e04597
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-15 21:41:04 +0530

    Time: 2 ms (99.37%) | Memory: 43.2 MB (31.24%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -14,18 +14,20 @@ class Solution {
 
     // base cases
     if (target == 0) {
-      if(!ans.contains(l))
-      ans.add(new ArrayList<Integer>(l));
+     // if (!ans.contains(l))
+        ans.add(new ArrayList<Integer>(l));
       return;
     }
 
-    for(int i=idx;i<candidates.length;i++){
-      if(i>idx && candidates[i]==candidates[i-1]) continue;
-      if(candidates[i]>target) break;
+    for (int i = idx; i < candidates.length; i++) {
+      if (i > idx && candidates[i] == candidates[i - 1])
+        continue;
+      if (candidates[i] > target)
+        break;
 
       l.add(candidates[i]);
-      helper(i+1,candidates,target-candidates[i],l);
-      l.remove(l.size()-1);
+      helper(i + 1, candidates, target - candidates[i], l);
+      l.remove(l.size() - 1);
     }
   }
 }
