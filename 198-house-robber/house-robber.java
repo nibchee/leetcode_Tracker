2cb71040176866ec1d15af0d31fb4fd885a30186
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-16 17:04:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (10.78%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,27 +1,30 @@
 class Solution {
-    int[] dp;
-    // private int helper(int i,int[] nums){
-    //     if(i==0) return dp[0]=nums[0];
-    //     if(i<0) return 0;
-    //     if(dp[i]!=0) return dp[i];
-    //     int cost1=helper(i-1,nums);
-    //     int cost2=helper(i-2,nums)+nums[i];
-    //     return dp[i]=Math.max(cost1,cost2);
-    // }
-     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1) return nums[0];
-         int prev1=nums[0];
-         int prev2=0,curr=0;
-        for(int i=1;i<n;i++){
-        int cost1=prev1;
-        int cost2=prev2+nums[i];   
-        curr=Math.max(cost1,cost2);
-        prev2=prev1;
-        prev1=curr; 
-        }
-        //return helper(n-1,nums);
-       // return dp[n-1];
-       return curr;
+  int dp[];
+
+  public int rob(int[] nums) {
+    int n = nums.length;
+    if(n==1) return nums[0];
+    dp = new int[n];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (int i = 2; i < n; i++) {
+      int prev = dp[i - 1];
+      int lastPrev =dp[i - 2] + nums[i];
+      dp[i] = Math.max(prev, lastPrev);
     }
+    return dp[n-1];
+    // return helper(n-1,nums);
+
+  }
+
+  private int helper(int i, int nums[]) {
+    if (i < 0) {
+      return 0;
+    }
+    if (dp[i] != 0)
+      return dp[i];
+    int prev = helper(i - 1, nums);
+    int lastPrev = helper(i - 2, nums) + nums[i];
+    return dp[i] = Math.max(prev, lastPrev);
+  }
 }
