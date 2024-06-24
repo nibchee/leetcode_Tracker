524bb3cfdbe0151ccf3c7ca8da6bd381b91d4337
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-25 00:51:37 +0530

    Time: 77 ms (93.85%) | Memory: 113.2 MB (34.84%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int minKBitFlips(vector<int>& nums, int k) {
-      int n=nums.size();
-      int flips=0;
-      int flipCountFromPastFori=0;
-      vector<bool> isFlipped(n,false);
+    int minKBitFlips(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        std::vector<int> totalFlipCount(n, 0);
+        totalFlipCount[0] = (nums[0] == 0) ? 1 : 0;
 
-      for(int i=0;i<n;i++){
-        if(i>=k && isFlipped[i-k]==true){
-          flipCountFromPastFori--;
+        for (int i = 1; i < n - k + 1; i++) {
+            int resultantFlips = totalFlipCount[i - 1];
+            if (i - k >= 0) {
+                resultantFlips -= totalFlipCount[i - k];
+            }
+            if ((nums[i] == 0 && resultantFlips % 2 == 0) || (nums[i] == 1 && resultantFlips % 2 == 1)) {
+                totalFlipCount[i] = totalFlipCount[i - 1] + 1;
+            } else {
+                totalFlipCount[i] = totalFlipCount[i - 1];
+            }
         }
 
-        if(flipCountFromPastFori%2==nums[i]){
-          if(i+k>n)
-          return -1;
-
-          flipCountFromPastFori++;
-          flips++;
-          isFlipped[i]=true;
+        for (int i = n - k + 1; i < n; i++) {
+            int resultantFlips = totalFlipCount[i - 1];
+            if (i - k >= 0) {
+                resultantFlips -= totalFlipCount[i - k];
+            }
+            if ((nums[i] == 0 && resultantFlips % 2 == 0) || (nums[i] == 1 && resultantFlips % 2 == 1)) {
+                return -1;
+            }
+            totalFlipCount[i] = totalFlipCount[i - 1];
         }
-      }
 
-      return flips;  
+        return totalFlipCount[n - 1];
     }
 };
