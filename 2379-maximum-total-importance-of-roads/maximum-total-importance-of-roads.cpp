e84ea59fea1be070187330e482c261c47a625c7b
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-28 21:33:14 +0530

    Time: 508 ms (12.76%) | Memory: 164.9 MB (22.42%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<long long> degree(n, 0);
-
-        for (vector<int>& edge : roads) {
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+        vector<int>adj[n];
+        for(int i=0;i<roads.size();i++){
+            adj[roads[i][0]].push_back(roads[i][1]);
+            adj[roads[i][1]].push_back(roads[i][0]);
         }
-
-        sort(degree.begin(), degree.end());
-
-        long long value = 1;
-        long long totalImportance = 0;
-        for (long long d : degree) {
-            totalImportance += (value * d);
-            value++;
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<n;i++){
+            int c=adj[i].size();    
+            pq.push({c,i});
         }
-
-        return totalImportance;
+        int c=n;
+        map<int,int>m;
+        cout<<n<<" "<<pq.size();
+        int k=n;
+        for(int i=0;i<k;i++){
+            pair<int,int>p=pq.top();
+            m[p.second]=n;
+            n--;
+            pq.pop();
+        }
+        long long sum=0;
+        for(int i=0;i<roads.size();i++){
+            sum+=m[roads[i][0]]+m[roads[i][1]];
+        }
+        return sum;
     }
 };
