aa49b0aadaa152c945584088469025c80913c3aa
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-29 14:32:48 +0530

    Time: 479 ms (10.10%) | Memory: 144.6 MB (33.81%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void findAncestors(map<int, vector<int>>& adjList, int node, set<int>& ancestors) {
+        for (auto parent : adjList[node]) {
+            if (ancestors.insert(parent).second) {  // Only recurse if parent is newly inserted
+                findAncestors(adjList, parent, ancestors);
+            }
+        }
+    }
+
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        map<int, vector<int>> adjList;
+        for (const auto& edge : edges) {
+            adjList[edge[1]].push_back(edge[0]);
+        }
+
+        vector<vector<int>> result(n);
+        for (int i = 0; i < n; ++i) {
+            set<int> ancestors;
+            findAncestors(adjList, i, ancestors);
+            result[i] = vector<int>(ancestors.begin(), ancestors.end());
+        }
+
+        return result;
+    }
+};
