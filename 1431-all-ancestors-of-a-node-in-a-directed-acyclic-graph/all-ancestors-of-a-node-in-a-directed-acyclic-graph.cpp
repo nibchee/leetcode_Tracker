3ed22e434e96ffdee6b648c63b5b8d80c321033b
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-29 14:57:50 +0530

    Time: 353 ms (36.66%) | Memory: 143.2 MB (43.47%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    void findAncestors(vector<vector<int>>& adjList, int node, set<int>& ancestors) {
+//    map<int,vector<int>>dp(1001);
+   void findAncestors(vector<vector<int>>& adjList, int node, set<int>& ancestors) {
         for (auto parent : adjList[node]) {
             if (ancestors.insert(parent).second) {  // Only recurse if parent is newly inserted
                 findAncestors(adjList, parent, ancestors);
             }
         }
     }
-
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adjList(n);
-        for ( auto& edge : edges) {
-            adjList[edge[1]].push_back(edge[0]);
+        //vector<set<int>>ans;
+        vector<vector<int>>m(n);
+        
+        //adjacency list
+        for(int i=0;i<edges.size();i++){
+            m[edges[i][1]].push_back(edges[i][0]);
         }
-
-        vector<vector<int>> result(n);
-        for (int i = 0; i < n; ++i) {
-            set<int> ancestors;
-            findAncestors(adjList, i, ancestors);
-            result[i] = vector<int>(ancestors.begin(), ancestors.end());
+        //finding parent
+        vector<vector<int>> req(n);
+        for(int i=0;i<n;i++){
+            set<int>s;
+            findAncestors(m,i,s);
+            //ans.push_back(s);
+             req[i]=vector<int>(s.begin(), s.end());
         }
-
-        return result;
+        //converting into required data type for returning answer
+        // vector<vector<int>> req;
+        // for(int i=0;i<ans.size();i++){
+        //     vector<int>v;
+        //     for(auto j:ans[i]){
+        //         v.push_back(j);
+        //     }
+        //     req.push_back(v);
+        // }
+        return req;
     }
 };
