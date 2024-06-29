d945470c78e01bf01a22aef82c2c0b66447ef769
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-06-29 18:04:43 +0530

    Time: 365 ms (33.59%) | Memory: 142.6 MB (46.21%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,59 +1,32 @@
 class Solution {
 public:
-//    map<int,vector<int>>dp(1001);
-vector<set<int>> dp;
-    void findp(map<int,vector<int>>m,int i,set<int>&s){
-      if(!dp[i].empty()){
-        s.insert(dp[i].begin(),dp[i].end());
-        return;
-      }
-        for(auto j:m[i]){
-            if(s.insert(j).second){
-            findp(m,j,s);
-            }
-        }
-        dp[i]=s;
-        //return ;
-    }
-
-      void findp(vector<vector<int>>&m,int i,set<int>&s){
-        // if(m[i].size()==0){
-        //     return ;
+    void findp(vector<int> m[], int i, set<int>& s, set<int> dp[]) {
+        // if (!dp[i].empty()) {
+        //     s.insert(dp[i].begin(), dp[i].end());
+        //     return;
         // }
-        for(auto j:m[i]){
-            if(s.insert(j).second)
-            findp(m,j,s);
+
+        for (auto j : m[i]) {
+            if (s.insert(j).second)
+                findp(m, j, s, dp);
         }
-      //  return ;
+        //  dp[i] = s;
     }
 
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        //vector<set<int>>ans;
-        //map<int,vector<int>>m(n);
-        vector<vector<int>>v(n);
-        //adjacency list
-        for(int i=0;i<edges.size();i++){
-           // m[edges[i][1]].push_back(edges[i][0]);
+        vector<int> v[n];
+        // adjacency list
+        for (int i = 0; i < edges.size(); i++) {
             v[edges[i][1]].push_back(edges[i][0]);
         }
-        //finding parent
-        vector<vector<int>> req(n);
-        for(int i=0;i<n;i++){
-            set<int>s;
-            //findp(m,i,s);
-            findp(v,i,s);
-            //ans.push_back(s);
-             req[i]=vector<int>(s.begin(), s.end());
+        set<int> dp[n];
+        // finding parent
+        vector<vector<int>> ans(n);
+        for (int i = 0; i < n; i++) {
+            set<int> s;
+            findp(v, i, s, dp);
+            ans[i] = vector<int>(s.begin(), s.end());
         }
-        //converting into required data type for returning answer
-        // vector<vector<int>> req;
-        // for(int i=0;i<ans.size();i++){
-        //     vector<int>v;
-        //     for(auto j:ans[i]){
-        //         v.push_back(j);
-        //     }
-        //     req.push_back(v);
-        // }
-        return req;
+        return ans;
     }
 };
