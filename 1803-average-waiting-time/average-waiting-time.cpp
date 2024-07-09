db3a028af10327f53b4908fd48806b310f299ff2
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-07-09 14:49:52 +0530

    Time: 143 ms (76.47%) | Memory: 76.6 MB (57.65%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-       int n = customers.size();
-          int start[n]; 
-        int finish[n] ;
+        int n = customers.size();
+        int start;
+        int finish;
         long total_waiting_time = 0;
 
-        start[0] = customers[0][0];
-        finish[0] = start[0] + customers[0][1];
-        total_waiting_time = finish[0] - start[0];
-        
+        start = customers[0][0];
+        finish = start + customers[0][1];
+        total_waiting_time = finish - start;
+
         for (int i = 1; i < n; i++) {
-            if (finish[i - 1] > customers[i][0]) {
-                start[i] = finish[i - 1];
+            if (finish > customers[i][0]) {
+                start = finish;
             } else {
-                start[i] = customers[i][0];
+                start = customers[i][0];
             }
-            finish[i] = start[i] + customers[i][1];
-            total_waiting_time += (finish[i] - customers[i][0]);
+            finish = start + customers[i][1];
+            total_waiting_time += (finish - customers[i][0]);
         }
-        
+
         double ans = (double)total_waiting_time / n;
 
-       
         return ans;
     }
 };
