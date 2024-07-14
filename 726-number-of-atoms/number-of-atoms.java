f5c1c307c6b8b3d65a5f22d88c9540cac0f4e91e
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-07-14 20:08:48 +0530

    Time: 9 ms (12.67%) | Memory: 42.2 MB (11.09%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.java
+++ 726-number-of-atoms/number-of-atoms.java
@@ -1,6 +1,6 @@
 class Solution {
    public String countOfAtoms(String formula) {
-        Deque<Map<String, Integer>> stack = new ArrayDeque<>();
+        Stack<Map<String, Integer>> stack = new Stack<>();
         stack.push(new HashMap<>());
         int n = formula.length();
         int i = 0;
@@ -13,11 +13,12 @@ class Solution {
             } else if (c == ')') {
                 Map<String, Integer> top = stack.pop();
                 i++;
-                int countStart = i;
+                String countStr="";
                 while (i < n && Character.isDigit(formula.charAt(i))) {
+                    countStr+=formula.charAt(i);
                     i++;
                 }
-                int count = countStart < i ? Integer.parseInt(formula.substring(countStart, i)) : 1;
+                int count =countStr==""? 1:Integer.parseInt(countStr);
                 Map<String, Integer> current = stack.peek();
                 for (String atom : top.keySet()) {
                     current.put(atom, current.getOrDefault(atom, 0) + top.get(atom) * count);
@@ -26,14 +27,17 @@ class Solution {
                 int start = i;
                 i++;
                 while (i < n && Character.isLowerCase(formula.charAt(i))) {
+                   // atom+=formula.charAt(i);
                     i++;
                 }
                 String atom = formula.substring(start, i);
-                int countStart = i;
+                  String countStr="";
                 while (i < n && Character.isDigit(formula.charAt(i))) {
+                    countStr+=formula.charAt(i);
                     i++;
                 }
-                int count = countStart < i ? Integer.parseInt(formula.substring(countStart, i)) : 1;
+                int count =countStr==""? 1:Integer.parseInt(countStr);
+              
                 Map<String, Integer> current = stack.peek();
                 current.put(atom, current.getOrDefault(atom, 0) + count);
             }
