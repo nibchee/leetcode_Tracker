9320ee5d34ae810a65a444c7a36136a68a112b77
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-07-22 21:17:58 +0530

    Time: 4 ms (73.20%) | Memory: 15.4 MB (5.97%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,48 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-
-using namespace std;
+bool comparator(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.first == b.first) ? a.second < b.second : a.first > b.first;
+}
 
 class Solution {
 public:
-    // Comparator function
-    static bool comparator(const pair<int, int>& a, const pair<int, int>& b) {
-        return (a.first == b.first) ? a.second < b.second : a.first > b.first;
-    }
-    
-    vector<int> frequencySort(vector<int>& nums) {
-        // Using a lambda function for the custom comparator in priority queue
-        auto comp = [](const pair<int, int>& a, const pair<int, int>& b) {
-            return comparator(a, b);
-        };
+    
 
-        // min heap with custom comparator
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> minH(comp);
-        unordered_map<int, int> mp;
-        vector<int> ans;
-        
-        // Count frequency of each element
-        for(int i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++;
-        }
-        
-        // Push elements into the heap
-        for(auto& i : mp) {
-            minH.push({i.second, i.first});
-        }
-        
-        // Pop elements from the heap and build the result
-        while(!minH.empty()) {
-            int freq = minH.top().first;
-            int num = minH.top().second;
-            for(int i = 0; i < freq; i++) {
-                ans.push_back(num);
-            }
-            minH.pop();
-        }
-        
-        return ans;
-    }
+    vector<int> frequencySort(vector<int>& nums) {
+        //kam freq ko upr rkhna hai to min heap bnayege
+         priority_queue<pair<int, int>, vector<pair<int, int>>, bool(*)(const pair<int, int>&, const pair<int, int>&)> minH(comparator);
+        unordered_map<int,int>mp;
+     
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto i:mp){
+            minH.push({i.second,i.first});
+        }
+           
+        while(minH.size()>0)
+     {
+            cout<<minH.top().second<<" ";
+            for(int i=0;i<minH.top().first;i++)
+          ans.push_back(minH.top().second);
+        
+        minH.pop();
+     }
+
+        return ans;
+    }
 };
