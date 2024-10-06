b45b949aae2bda27e5e9df1e2ee7a12ef79fbd27
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-10-06 22:43:20 +0530

    Time: 4 ms (100.00%) | Memory: 43.8 MB (64.03%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        //If  a number is negative number
-        //if the number end with 0 except the number 0
-        if(x<0 || (x!=0 && x%10==0)){
-            return false;
+        if(x<0) return false;
+        int n1=x;
+        int n2=0;
+        while(x!=0){
+          n2=n2*10+(x%10);
+          x=x/10;
         }
-
-        int reverse=0;
-        int num=x;
-        while(num>reverse){
-            int lastDigit=num%10;
-            reverse=reverse*10+lastDigit;
-           num=num/10;
-        }
-        //num==reverse/10 is for Odd digits & else for even digits
-        return num==reverse/10 || num==reverse;
-        
+        return (n2==n1);
     }
 }
-//TC:O(logN)
