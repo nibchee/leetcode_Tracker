5c57e3776258713137675f75ad454a1d8e1c405d
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-11-11 17:12:36 +0530

    Time: 11 ms (10.53%) | Memory: 44.8 MB (33.77%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -8,6 +8,19 @@ class Solution {
         return true;  
     }
     public boolean primeSubOperation(int[] nums) {
+        int maxEle=Integer.MIN_VALUE;
+         for(int num: nums){
+            maxEle=Math.max(maxEle,num);
+         }
+
+         int[] prevPrime=new int[maxEle+1];
+         for(int i=2;i<=maxEle;i++){
+            if(isPrime(i)){
+                prevPrime[i]=i;
+            }else{
+                prevPrime[i]=prevPrime[i-1];
+            }
+         }
          int prime_range=nums[0];
         for(int i=0;i<nums.length;i++){
            if(i!=0)
@@ -16,16 +29,11 @@ class Solution {
             if(prime_range<=0)
             return false;
 
-            int nearest_prime=0;
-            for(int j=prime_range-1;j>=2;j--){
-                if(isPrime(j)){
-                   nearest_prime=j;
-                   break; 
-                }
-            }
+            int nearest_prime=prevPrime[prime_range-1];
+            
 
             nums[i]=nums[i]-nearest_prime;
-
+            
         }
         return true;
     }
