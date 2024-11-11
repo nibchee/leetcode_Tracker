1eb9f4632a790e337c5afe0608dad712b1181159
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-11-11 16:57:34 +0530

    Time: 3 ms (65.79%) | Memory: 44.4 MB (70.20%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -0,0 +1,32 @@
+class Solution {
+    private boolean isPrime(int n){
+       for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;  
+    }
+    public boolean primeSubOperation(int[] nums) {
+         int prime_range=nums[0];
+        for(int i=0;i<nums.length;i++){
+           if(i!=0)
+            prime_range=nums[i]-nums[i-1];
+
+            if(prime_range<=0)
+            return false;
+
+            int nearest_prime=0;
+            for(int j=prime_range-1;j>=2;j--){
+                if(isPrime(j)){
+                   nearest_prime=j;
+                   break; 
+                }
+            }
+
+            nums[i]=nums[i]-nearest_prime;
+
+        }
+        return true;
+    }
+}
