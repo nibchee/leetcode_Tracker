c36fd4eac5dccc5854b40cd7874fd294340987a9
GeekyNib <51364680+nibchee@users.noreply.github.com>
2024-12-12 13:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (44.13%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,20 @@
 class Solution {
-    private void dfs(int [][]image,int cr, int cc,int color,int pixel_color){
-        //System.out.println(cr+" "+cc);
-     if(cr<0 || cc<0 || cr>image.length-1 || cc>image[cr].length-1 || image[cr][cc]==color || image[cr][cc]!=pixel_color){
-        return;
-     }
-    
-     image[cr][cc]=color;
-     dfs(image,cr,cc+1,color,pixel_color);
-     dfs(image,cr-1,cc,color,pixel_color);
-     dfs(image,cr,cc-1,color,pixel_color);
-     dfs(image,cr+1,cc,color,pixel_color);
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int pixel_color=image[sr][sc];
-        if(color!=pixel_color)
-        dfs(image,sr,sc,color,pixel_color);
+       int oldcolor=image[sr][sc];
+        dfs(image,sr,sc,color,oldcolor);
         return image;
     }
+
+    private void dfs(int[][]img,int i,int j,int color,int oldcolor){
+        if(i<0 || i>=img.length || j<0 || j>=img[0].length || img[i][j]==color|| img[i][j]!=oldcolor)
+        return;
+
+        img[i][j]=color;
+        dfs(img,i-1,j,color,oldcolor);
+        dfs(img,i+1,j,color,oldcolor);
+        dfs(img,i,j-1,color,oldcolor);
+        dfs(img,i,j+1,color,oldcolor);
+    }
+
+
 }
