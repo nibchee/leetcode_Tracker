38b80018c67216ce2b5c6b976121ad84a4325ca3
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-01-04 04:37:47 +0530

    Time: 4 ms (32.12%) | Memory: 40.8 MB (38.58%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,34 +1,17 @@
 class Solution {
-  public int findTheWinner(int n, int k) {
-    int p[] = new int[n];
-    int currPos = 0;
-    int ones = 0;
-    while (ones < n - 1) {
-      int c = 0;
-      while (c < k) {
-        currPos = (currPos + n) % n;
-        if (p[currPos] == 0)
-          c++;
-       // System.out.println(currPos + " " + ones+" "+c);
-        currPos++;
-      }
-     currPos = (currPos + n-1) % n;
-      p[currPos] = 1;
-      ones++;
-     // System.out.println(currPos + " " + ones);
-      while (p[currPos] == 1) {
-        currPos++;
-        currPos = (currPos + n) % n;
-      }
-  //System.out.println(currPos + " " + ones);
+    public int findTheWinner(int n, int k) {
+        List<Integer> friends = new LinkedList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            friends.add(i);
+        }
+
+        int i=0;
+        while (friends.size()!=1){
+            int  killedFriendPos = (i+k-1) % friends.size();
+            friends.remove(killedFriendPos);
+            i =killedFriendPos ;
+        }
+
+        return friends.get(0);
     }
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-      if (p[i] == 0) {
-        ans = i + 1;
-        break;
-      }
-    }
-    return ans;
-  }
 }
