13aa5221669de46839801767c519f107e8b279f8
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-03-15 05:51:29 +0530

    Time: 20 ms (40.30%) | Memory: 60.2 MB (25.87%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int minCapability(int[] nums, int k) {
+        int minReward = 1;
+        int maxReward = Arrays.stream(nums).max().getAsInt();
+        int totalHouses = nums.length;
+
+        while (minReward < maxReward) {
+            int midReward = (minReward + maxReward) / 2;
+            int possibleThefts = 0;
+
+            for (int index = 0; index < totalHouses; ++index) {
+                if (nums[index] <= midReward) {
+                    possibleThefts += 1;
+                    index++;                 }
+            }
+
+            if (possibleThefts >= k) maxReward = midReward;
+            else minReward = midReward + 1;
+        }
+
+        return minReward;
+    }
+}
