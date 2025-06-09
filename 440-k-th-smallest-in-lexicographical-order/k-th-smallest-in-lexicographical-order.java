4d3594ebcce5321d77b748a0dc7b4a0c3a78c4bf
GeekyNib <51364680+nibchee@users.noreply.github.com>
2025-06-09 09:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (63.26%) - LeetSync

--- 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.java
+++ 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.java
@@ -6,10 +6,13 @@ class Solution {
 
         while (k > 0) {
             int step = countSteps(n, curr, curr + 1);
+            // If the steps are less than or equal to k, we skip this prefix's subtree
             if (step <= k) {
+                // Move to the next prefix and decrease k by the number of steps we skip
                 curr++;
                 k -= step;
             } else {
+                // Move to the next level of the tree and decrement k by 1
                 curr *= 10;
                 k--;
             }
@@ -18,6 +21,7 @@ class Solution {
         return curr;
     }
 
+    // To count how many numbers exist between prefix1 and prefix2
     private int countSteps(int n, long prefix1, long prefix2) {
         int steps = 0;
         while (prefix1 <= n) {
